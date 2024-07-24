a0d60e71087b32267c8f243fcf3e95ff45f50c3c
Prashannakafle4444 <kafleprakrit@gmail.com>
2024-07-24 16:48:12 +0545

    user chat box started

--- client/src/components/ChatPage/ChatPage.tsx
+++ client/src/components/ChatPage/ChatPage.tsx
@@ -11,8 +11,21 @@ export const ChatPage = () => {
                   <img src="/img/pp.jpg" alt="profilepic"></img>
                 </div>
                 <span className="username">UserName</span>
-
                 <div className="downarrow">&darr;</div>
+              </div>
+              <div className="svg-container ">
+                <img
+                  className="svg"
+                  src="img/audiocall.svg"
+                  alt="audiocall"
+                ></img>
+                <img
+                  className="svg"
+                  src="img/videocall.svg"
+                  alt="videocall"
+                ></img>
+                <img className="svg" src="img/minimize.svg" alt="mini"></img>
+                <img className="svg" src="img/close.svg" alt="close"></img>
               </div>
             </div>
           </div>
